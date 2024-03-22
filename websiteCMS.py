39801cfbd4d3a0f0f4e446c392684ab6342e9ffb
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-03-22 16:40:17 -0500

    Basic Content Management System Framework
    Created the outline for the CMS system using the DearPyGUI Framework.

---
+++ websiteCMS.py
@@ -0,0 +1,30 @@
+# Project   : Website CMS Python
+# Title     : Website CMD Main
+# Author    : Tej Pandit
+# Date      : March 2024
+
+import dearpygui.dearpygui as dpg
+from LoginManager import LoginManager
+from ContentManagementSystem import ContentManagementSystem
+from DataManager import DataManager
+
+dpg.create_context()
+dpg.create_viewport(title='NUAI Lab Website Manager', width=1200, height=700)
+
+cms = ContentManagementSystem()
+dataman = DataManager()
+#login = LoginManager()
+
+#login.dataman = dataman
+#login.cms = cms
+
+# TESTING : LOAD LOCAL DATA
+dataman.loadLocalData()
+
+cms.createCMSWindow()
+#login.createLoginWindow()
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.start_dearpygui()
+dpg.destroy_context()
