440526e019bfad81cb208e171c91cb6c99c6e193
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-09-01 22:11:10 -0500

    Custom Theme for PyCMS
    Created a custom theme template for DPG for the PyCMS App.

---
+++ Custom_Themes.py
@@ -0,0 +1,13 @@
+# Project   : Website CMS Python
+# Title     : Custom Themes
+# Author    : Tej Pandit
+# Date      : Sept 2024
+
+from typing import List, Any, Callable, Union
+import dearpygui.dearpygui as dpg
+
+class Custom_Themes():
+
+    def __init__(self):
+        pass
+    
