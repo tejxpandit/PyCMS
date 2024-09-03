5973e5eb6bd62058fb66cf8fc46c7607de4aa4bf
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-09-02 20:28:26 -0500

    Edit Button Theme
    Added the theming for the edit button.

--- Custom_Themes.py
+++ Custom_Themes.py
@@ -11,3 +11,12 @@ class Custom_Themes():
     def __init__(self):
         pass
     
+    # THEME : Edit Button
+    def EditTheme(self) -> Union[str, int]:
+        with dpg.theme() as theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text                   , (30, 30, 30, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Button                 , (83, 204, 105, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered          , (182, 247, 194, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive           , (83, 204, 105, 150))
+        return theme
