5dbecf0ab91d585b08321a4267bc3f4e816820ee
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-09-03 23:52:47 -0500

    Save Button Custom Theme
    A custom theme for the save button.

--- Custom_Themes.py
+++ Custom_Themes.py
@@ -40,3 +40,13 @@ class Custom_Themes():
                 dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered          , (209, 35, 94, 255))
                 dpg.add_theme_color(dpg.mvThemeCol_ButtonActive           , (138, 20, 60, 150))
         return theme
+    
+    # THEME : Save Button
+    def SaveTheme(self) -> Union[str, int]:
+        with dpg.theme() as theme:
+            with dpg.theme_component(0):
+                dpg.add_theme_color(dpg.mvThemeCol_Text                   , (255, 255, 255, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_Button                 , (22, 107, 38, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonHovered          , (30, 158, 53, 255))
+                dpg.add_theme_color(dpg.mvThemeCol_ButtonActive           , (22, 107, 38, 150))
+        return theme
