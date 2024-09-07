25a26ebabb468c4db4ca96ba5a8682e792c0d930
Tej Pandit <67128950+tejxpandit@users.noreply.github.com>
2024-09-06 23:58:55 -0500

    Added Dark Theme for DPG
    Added the dark theme (pre-defined) for DPG apps.

---
+++ Dark_Theme.py
@@ -0,0 +1,11 @@
+# Project   : DearPyGUI External Resources
+# Title     : Global Dark Theme
+# Author    : Jonathan Hoffstadt
+# Repo      : https://github.com/hoffstadt/DearPyGui_Ext/blob/master/dearpygui_ext/themes.py
+# License   : MIT
+
+from typing import List, Any, Callable, Union
+import dearpygui.dearpygui as dpg
+
+def createDarkTheme() -> Union[str, int]:
+    pass
